0338a9ac524d9892bf4e51ca7bae9ff61659ad6a
Michał Sidor (Michcioperz) <public+git@meekchopp.es>
2017-07-06 20:37:20 +0200

    now loading targets from jsons

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"strconv"
 )
+
+const TARGETS_DIR = "targets"
 
 type Block struct {
 	Description, Query string
@@ -53,507 +59,7 @@ type Subject struct {
 	Extended           bool
 }
 
-var Targets = []Target{
-	{
-		School:     "Uniwersytet Warszawski",
-		Department: "Wydział Matematyki, Informatyki i Mechaniki",
-		Class:      "Informatyka",
-		KnownFrontiers: []Frontier{
-			{
-				Year:  2014,
-				Value: 84.18,
-			},
-			{
-				Year:  2015,
-				Value: 86.64,
-			},
-			{
-				Year:  2016,
-				Value: 85.44,
-			},
-		},
-		Blocks: []Block{
-			{
-				Description: "język polski",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.6 END) * 0.1 AS score
-FROM results
-WHERE subject = 'język polski'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "matematyka",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.6 END) * 0.1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy nowożytny",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.6 END) * 0.1 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-			{
-				Description: "rozszerzona matematyka lub informatyka",
-				Query: `
-SELECT
-  *,
-  percentage * 1 * 0.5 AS score
-FROM results
-WHERE extended = 1 AND subject IN ('matematyka', 'informatyka')
-ORDER BY score DESC`,
-			},
-			{
-				Description: "dowolne rozszerzenie",
-				Query: `
-SELECT
-  *,
-  percentage * 1 * 0.2 AS score
-FROM results
-WHERE extended = 1
-ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Lubelska",
-		Department: "Wydział Elektrotechniki i Informatyki",
-		Class:      "Informatyka",
-		Blocks: []Block{
-			{
-				Description: "język polski",
-				Query: `
-SELECT
-  *,
-  (CASE WHEN (extended = 1 AND percentage >= 30) THEN percentage + (6*percentage + 100)/7 ELSE percentage END) * 0.1 AS score
-FROM results
-WHERE subject = 'język polski'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy nowożytny",
-				Query: `
-SELECT
-  *,
-  (CASE WHEN (extended = 1 AND percentage >= 30) THEN percentage + (6*percentage + 100)/7 ELSE percentage END) * 0.3 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-			{
-				Description: "przedmiot magiczny",
-				Query: `SELECT *
-FROM (SELECT
-        *,
-        (CASE WHEN (extended = 1 AND percentage >= 30)
-          THEN percentage + (6 * percentage + 100) / 7
-         ELSE percentage END) AS score
-      FROM results
-      WHERE subject IN ('matematyka', 'fizyka', 'informatyka')
-      UNION
-      SELECT
-        *,
-        (CASE WHEN (extended = 1 AND percentage >= 30)
-          THEN (6 * percentage + 100) / 7
-         ELSE percentage END) AS score
-      FROM results
-      WHERE subject IN ('historia', 'biologia', 'chemia', 'geografia', 'wiedza o społeczeństwie'))
-ORDER BY score
-  DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Łódzka",
-		Department: "*",
-		Class:      "Informatyka",
-		Blocks: []Block{
-			{
-				Description: "matematyka",
-				Query:       `SELECT *, (CASE WHEN percentage < 30 THEN 0 ELSE percentage*(CASE WHEN extended = 1 THEN 5 ELSE 2 END) END) AS score FROM results WHERE subject = 'matematyka' ORDER BY score DESC`,
-			},
-			{
-				Description: "fizyka/chemia/informatyka",
-				Query:       `SELECT *, (CASE WHEN percentage < 30 THEN 0 ELSE percentage*(CASE WHEN extended = 1 THEN 3 ELSE 1.2 END) END) AS score FROM results WHERE subject IN ('fizyka', 'chemia', 'informatyka') ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy",
-				Query:       `SELECT *, (CASE WHEN percentage < 30 THEN 0 ELSE percentage*(CASE WHEN extended = 1 THEN 2 ELSE 0.8 END) END) AS score FROM results WHERE subject IN ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski') ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Uniwersytet Marii Curie-Skłodowskiej w Lublinie",
-		Department: "Wydział Matematyki, Fizyki i Informatyki",
-		Class:      "Informatyka",
-		Blocks: []Block{
-			{
-				Description: "matematyka * 1",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 2 ELSE 1 END) * 1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "fizyka * 0.6",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 2 ELSE 1 END) * 0.6 AS score
-FROM results
-WHERE subject = 'fizyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "informatyka * 0.6",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 2 ELSE 1 END) * 0.6 AS score
-FROM results
-WHERE subject = 'informatyka'
-ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Warszawska",
-		Department: "Wydział Elektroniki i Technik Informacyjnych",
-		Class:      "Informatyka",
-		KnownFrontiers: []Frontier{
-			{
-				Year:  2013,
-				Value: 179,
-			},
-			{
-				Year:  2014,
-				Value: 173,
-			},
-			{
-				Year:  2015,
-				Value: 178,
-			},
-			{
-				Year:  2016,
-				Value: 184,
-			},
-		},
-		Blocks: []Block{
-			{
-				Description: "matematyka",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "wybrany przedmiot",
-				Query: `SELECT
-  *,
-  percentage * (CASE WHEN extended = 1
-    THEN 1
-                ELSE 0.5 END) * (CASE subject
-                                 WHEN 'fizyka'
-                                   THEN 1
-                                 WHEN 'informatyka'
-                                   THEN 1
-                                 WHEN 'chemia'
-                                   THEN 0.75
-                                 WHEN 'biologia'
-                                   THEN 0.5
-                                 ELSE 0 END) AS score
-                                            FROM results
-                                            WHERE subject IN ('fizyka', 'informatyka', 'chemia', 'biologia')
-                                            ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy * 0.25",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 0.25 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Warszawska",
-		Department: "Wydział Elektryczny",
-		Class:      "Informatyka",
-		KnownFrontiers: []Frontier{
-			{
-				Year:  2013,
-				Value: 153,
-			},
-			{
-				Year:  2014,
-				Value: 158,
-			},
-			{
-				Year:  2015,
-				Value: 166,
-			},
-			{
-				Year:  2016,
-				Value: 173,
-			},
-		},
-		Blocks: []Block{
-			{
-				Description: "matematyka",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "wybrany przedmiot",
-				Query: `SELECT
-  *,
-  percentage * (CASE WHEN extended = 1
-    THEN 1
-                ELSE 0.5 END) * (CASE subject
-                                 WHEN 'fizyka'
-                                   THEN 1
-                                 WHEN 'informatyka'
-                                   THEN 1
-                                 WHEN 'chemia'
-                                   THEN 0.75
-                                 WHEN 'biologia'
-                                   THEN 0.5
-                                 ELSE 0 END) AS score
-                                            FROM results
-                                            WHERE subject IN ('fizyka', 'informatyka', 'chemia', 'biologia')
-                                            ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy * 0.25",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 0.25 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Warszawska",
-		Department: "Wydział Matematyki i Nauk Informacyjnych",
-		Class:      "Informatyka",
-		KnownFrontiers: []Frontier{
-			{
-				Year:  2013,
-				Value: 182,
-			},
-			{
-				Year:  2014,
-				Value: 180,
-			},
-			{
-				Year:  2015,
-				Value: 182,
-			},
-			{
-				Year:  2016,
-				Value: 194,
-			},
-		},
-		Blocks: []Block{
-			{
-				Description: "matematyka",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "wybrany przedmiot",
-				Query: `SELECT
-  *,
-  percentage * (CASE WHEN extended = 1
-    THEN 1
-                ELSE 0.5 END) * (CASE subject
-                                 WHEN 'fizyka'
-                                   THEN 1
-                                 WHEN 'informatyka'
-                                   THEN 1
-                                 WHEN 'chemia'
-                                   THEN 0.75
-                                 WHEN 'biologia'
-                                   THEN 0.5
-                                 ELSE 0 END) AS score
-                                            FROM results
-                                            WHERE subject IN ('fizyka', 'informatyka', 'chemia', 'biologia')
-                                            ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy * 0.25",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 0.25 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Warszawska",
-		Department: "Wydział Elektroniki i Technik Informacyjnych",
-		Class:      "Computer Science",
-		KnownFrontiers: []Frontier{
-			{
-				Year:  2015,
-				Value: 124,
-			},
-			{
-				Year:  2016,
-				Value: 145,
-			},
-		},
-		Blocks: []Block{
-			{
-				Description: "matematyka",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "wybrany przedmiot",
-				Query: `SELECT
-  *,
-  percentage * (CASE WHEN extended = 1
-    THEN 1
-                ELSE 0.5 END) * (CASE subject
-                                 WHEN 'fizyka'
-                                   THEN 1
-                                 WHEN 'informatyka'
-                                   THEN 1
-                                 WHEN 'chemia'
-                                   THEN 0.75
-                                 WHEN 'biologia'
-                                   THEN 0.5
-                                 ELSE 0 END) AS score
-                                            FROM results
-                                            WHERE subject IN ('fizyka', 'informatyka', 'chemia', 'biologia')
-                                            ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy * 0.25",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 0.25 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-		},
-	},
-	{
-		School:     "Politechnika Warszawska",
-		Department: "Wydział Matematyki i Nauk Informacyjnych",
-		Class:      "Computer Science",
-		KnownFrontiers: []Frontier{
-			{
-				Year:  2013,
-				Value: 81,
-			},
-			{
-				Year:  2014,
-				Value: 130,
-			},
-			{
-				Year:  2015,
-				Value: 126,
-			},
-			{
-				Year:  2016,
-				Value: 157,
-			},
-		},
-		Blocks: []Block{
-			{
-				Description: "matematyka",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 1 AS score
-FROM results
-WHERE subject = 'matematyka'
-ORDER BY score DESC`,
-			},
-			{
-				Description: "wybrany przedmiot",
-				Query: `SELECT
-  *,
-  percentage * (CASE WHEN extended = 1
-    THEN 1
-                ELSE 0.5 END) * (CASE subject
-                                 WHEN 'fizyka'
-                                   THEN 1
-                                 WHEN 'informatyka'
-                                   THEN 1
-                                 WHEN 'chemia'
-                                   THEN 0.75
-                                 WHEN 'biologia'
-                                   THEN 0.5
-                                 ELSE 0 END) AS score
-                                            FROM results
-                                            WHERE subject IN ('fizyka', 'informatyka', 'chemia', 'biologia')
-                                            ORDER BY score DESC`,
-			},
-			{
-				Description: "język obcy * 0.25",
-				Query: `
-SELECT
-  *,
-  percentage * (CASE WHEN extended = 1 THEN 1 ELSE 0.5 END) * 0.25 AS score
-FROM results
-WHERE subject IN
-      ('język angielski', 'język francuski', 'język niemiecki', 'język hiszpański', 'język włoski', 'język rosyjski')
-ORDER BY score DESC`,
-			},
-		},
-	},
-}
+var Targets = []Target{}
 
 var Subjects = []Subject{
 	{"pol", "język polski", false},
@@ -596,13 +102,42 @@ var Subjects = []Subject{
 var Templates = map[string]*template.Template{}
 
 func main() {
+	var err error
 	for _, templ_name := range []string{"block_foot.tpl.html", "block_head.tpl.html", "blocks_foot.tpl.html", "blocks_head.tpl.html", "fieldp.tpl.html", "fieldr.tpl.html", "foot.tpl.html", "frontier.tpl.html", "frontiers_foot.tpl.html", "frontiers_head.tpl.html", "head.tpl.html", "resultcard_foot.tpl.html", "resultcard_head.tpl.html", "score.tpl.html", "target_foot.tpl.html", "target_head.tpl.html"} {
-		var err error
 		Templates[templ_name], err = template.ParseFiles(templ_name)
 		if err != nil {
 			log.Panic(err)
 			return
 		}
+	}
+	targets, err := ioutil.ReadDir(TARGETS_DIR)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	for _, targetFileInfo := range targets {
+		var target = new(Target)
+		targetFileName := path.Join(TARGETS_DIR, targetFileInfo.Name())
+		log.Println("loading", targetFileName)
+		if path.Ext(targetFileName) == ".json" {
+			targetFile, err := os.Open(targetFileName)
+			if err != nil {
+				log.Println("skipping file for error", err)
+				continue
+			}
+			defer targetFile.Close()
+			dec := json.NewDecoder(targetFile)
+			err = dec.Decode(target)
+			if err != nil {
+				log.Println("skipping file for error", err)
+				continue
+			}
+			Targets = append(Targets, *target)
+		}
+	}
+	if len(Targets) < 1 {
+		log.Panic("no targets specified, what's the point")
+		return
 	}
 	http.HandleFunc("/oh-boi/", formReactor)
 	http.HandleFunc("/", formCreator)
